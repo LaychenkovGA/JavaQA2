39bddf9d42094ddb98ae99a30c0ea1cb74bb3041
Gleb Laychenkov <racers2no95@mail.ru>
2024-02-23 16:21:30 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int bilet = 13_676;
+        int mile = bilet / 20;
+
+        System.out.println("Стоимость билета " + bilet + " рублей");
+        System.out.println("Начислено " + mile + " бонусов");
+
+    }
+}
